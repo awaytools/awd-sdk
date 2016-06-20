050d78e29e6e3c033c599c0caf7ddafb64a85260
Robin Lockhart <80prozent@differentdesign.de>
2016-06-20 19:18:07 +0200

    mac update

--- cpp-libawd/include/utils/awd_globals.h
+++ cpp-libawd/include/utils/awd_globals.h
@@ -5,7 +5,7 @@ namespace AWD
 {
 	static const int VERSION_MAJOR = 3;		///< The Major version number of libAWD
 	static const int VERSION_MINOR = 0;		///< The Minor version number of libAWD
-	static const int VERSION_BUILD = 1;		///< The Build version number of libAWD
+	static const int VERSION_BUILD = 2;		///< The Build version number of libAWD
 	static const char VERSION_RELEASE = 'b';	///< The Release version char of libAWD
 
 	static const int RESSOURCE_LIMIT = 3 * 0xffffff;		///< The Maximal stream length in bytes \todo Make sure this is correct.
